8b400540789589ee26a2a0cafb39cca6515e9e49
xmoonanx <89209230+xmoonanx@users.noreply.github.com>
2026-01-01 20:16:11 +0900

    Title: 달에서 무게 구하기, Time: 93ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260101/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int w = 13;
+        double gr = 0.165;
+        double res = w * gr;
+        System.out.printf(w +" * %.6f" + " = %.6f", gr, res);
+    }
+}
