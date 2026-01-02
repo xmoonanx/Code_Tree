6f963f82a164ebfe1429fb9070613bcd6068de01
xmoonanx <89209230+xmoonanx@users.noreply.github.com>
2026-01-02 13:41:13 +0900

    Title: 점수 입출력, Time: 138ms, Memory: 10MB, Status: Passed - Codetree

--- 260102/점수 입출력/enter-int-and-print-score.java
+++ 260102/점수 입출력/enter-int-and-print-score.java
@@ -1,12 +1,12 @@
+        import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // Please write your code here.
-        import java.util.Scanner;
         Scanner sc = new Scanner(System.in);
         int age = sc.nextInt();
 
         if(age>=1 && age<=1000){
-            System.out.printf("Your score is %d point", age);
+            System.out.printf("Your score is %d point.", age);
         }
     }
 }
