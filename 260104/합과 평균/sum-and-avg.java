25e05811e8ee03da25ec9bc104c7dbfa20d3fcf7
xmoonanx <89209230+xmoonanx@users.noreply.github.com>
2026-01-04 11:52:20 +0900

    Title: 합과 평균, Time: 129ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260104/합과 평균/sum-and-avg.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int sum = a+b;
+        double avg = sum/2.0;
+
+        System.out.printf("%d %.1f", sum, avg);
+    }
+}
