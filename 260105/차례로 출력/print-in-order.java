b572f90d0f712151e94f84ca2a2ee68a6036b30b
xmoonanx <89209230+xmoonanx@users.noreply.github.com>
2026-01-05 15:29:46 +0900

    Title: 차례로 출력, Time: 92ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 260105/차례로 출력/print-in-order.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        for(int i= 5; i<18; i++){
+            System.out.print(i + " ");
+        }
+    }
+}
