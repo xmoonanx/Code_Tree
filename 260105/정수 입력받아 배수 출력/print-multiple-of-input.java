606f80399322a1ae1ca4bc68c9d38c276feb5962
xmoonanx <89209230+xmoonanx@users.noreply.github.com>
2026-01-05 18:52:27 +0900

    Title: 정수 입력받아 배수 출력, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260105/정수 입력받아 배수 출력/print-multiple-of-input.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 1; i<6; i++){
+            System.out.print(n*i + " ");
+        }
+    }
+}
