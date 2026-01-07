c26c7b69c14888b2ceee1bcac1592fd92dbfa706
xmoonanx <89209230+xmoonanx@users.noreply.github.com>
2026-01-07 10:55:20 +0900

    Title: 배열의 합 계산하기, Time: 107ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 260107/배열의 합 계산하기/calculate-sum-of-array.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int[] A = {3,1,4,5,9};
+        
+        int res = A[1] + A[3] + A[4];
+        System.out.println(res);
+    }
+}
