8dbe2fd476e25442e28ef4ea528296b9e90f724b
xmoonanx <89209230+xmoonanx@users.noreply.github.com>
2026-01-07 16:48:37 +0900

    Title: 친근하지 않은 수, Time: 114ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260107/친근하지 않은 수/unfriendly-number.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int res =0;
+        for(int i = 1; i<=N; i++){
+            if(i%2==0 || i%3==0 || i%5 ==0) continue;
+            res++;
+        }
+        // System.out.print(res);
+    }
+}
