45ce6e67840ecca8598535084d05353edba1f60a
xmoonanx <89209230+xmoonanx@users.noreply.github.com>
2026-01-13 11:15:24 +0900

    Title: 건너뛰고 별표 출력하기, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260113/건너뛰고 별표 출력하기/skip-and-print-a-star.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+            System.out.println();
+        }
+        for(int i = n-1; i>0; i--){
+            for(int j = i; j>0; j--){
+                System.out.print("*");
+            }
+            System.out.println();
+            System.out.println();
+        }
+    }
+}
