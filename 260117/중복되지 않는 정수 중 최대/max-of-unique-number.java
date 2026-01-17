33ef5d8753fc7e0f7c50b3c46fbbcc6477a3411c
xmoonanx <89209230+xmoonanx@users.noreply.github.com>
2026-01-17 19:49:06 +0900

    Title: 중복되지 않는 정수 중 최대, Time: 199ms, Memory: 12MB, Status: Passed - Codetree

--- 260117/중복되지 않는 정수 중 최대/max-of-unique-number.java
+++ 260117/중복되지 않는 정수 중 최대/max-of-unique-number.java
@@ -1,23 +1,41 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class Main {
+    public static final int MAX_N = 1000;
+
     public static void main(String[] args) {
-        // Please write your code here.
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int[] arr = new int[n];
 
-        int max = 0;
-        for(int i = 0 ; i<n; i++){
-            arr[i] = sc.nextInt();
-            if(arr[i] > max) max = arr[i];
+        int n;
+        int[] nums = new int[MAX_N];
+
+        // 입력
+        n = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            nums[i] = sc.nextInt();
+
+        // 최댓값 찾기
+        int max = -1;
+        for(int i = 0; i < n; i++) {
+            int currNum = nums[i];
+            // 최대가 될 수 있는 후보입니다.
+            if(max < currNum) {
+                // 갱신할 수 있는지 확인하기 위해 이 숫자의 등장 빈도를 셉니다.
+                int count = 0;
+                for(int j = 0; j < n; j++) {
+                    if(nums[j] == currNum) {
+                        count++;
+                    }
+                }
+                // 이 숫자가 배열에서 유일할때만 갱신합니다.
+                if(count == 1) {
+                    max = currNum;
+                }
+            }
         }
-        int cnt = 0;
-        for(int i = 0; i<n; i++){
-            if(arr[i] == max) cnt++;
-        }
 
-        if(cnt > 1) System.out.print(-1);
-        else System.out.print(max); 
-
+        // 최댓값과 최솟값을 출력합니다.
+        System.out.println(max);
     }
 }
+
