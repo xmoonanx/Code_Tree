4c70051fbcd9da680e21c9b1b965a38bcec494aa
xmoonanx <89209230+xmoonanx@users.noreply.github.com>
2026-01-19 12:36:12 +0900

    Title: 두 배열의 곱, Time: 124ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260119/두 배열의 곱/multiple-of-two-arrays.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int[][] arr1 = new int[3][3];
+        int[][] arr2 = new int[3][3];
+
+        for(int i =0 ;i<3; i++){
+            for(int j =0; j<3; j++){
+                arr1[i][j] = sc.nextInt();
+            }
+        }
+        for(int i =0 ;i<3; i++){
+            for(int j =0; j<3; j++){
+                arr2[i][j] = sc.nextInt();
+                arr2[i][j] = arr1[i][j] * arr2[i][j];
+                System.out.print(arr2[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
