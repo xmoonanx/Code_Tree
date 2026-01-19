37ba5bbba8bd18fce3a4856ad99f6417285bed73
xmoonanx <89209230+xmoonanx@users.noreply.github.com>
2026-01-19 12:27:31 +0900

    Title: 배열의 평균, Time: 132ms, Memory: 10MB, Status: Passed - Codetree

--- 260119/배열의 평균/ave-of-array.java
+++ 260119/배열의 평균/ave-of-array.java
@@ -17,7 +17,7 @@ public class Main {
             for(int j =0; j<4; j++){
                 col += arr[i][j];
             }
-            System.out.print(col/4.0 + " ");
+            System.out.printf("%.1f ",col/4.0);
         }
         System.out.println();
         for(int i = 0; i<4; i++){
@@ -25,10 +25,10 @@ public class Main {
             for(int j =0; j<2; j++){
                 row += arr[j][i];
             }
-            System.out.print(row/2.0 + " ");
+            System.out.printf("%.1f ",row/2.0);
         }
         System.out.println();
-        System.out.print(sum/8.0);
+        System.out.printf("%.1f ", sum/8.0);
 
         
 
