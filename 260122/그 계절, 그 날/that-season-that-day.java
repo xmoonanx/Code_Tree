021e14c178ed840e1d3f7c92d39e5c2dcb077371
xmoonanx <89209230+xmoonanx@users.noreply.github.com>
2026-01-22 15:04:28 +0900

    Title: 그 계절, 그 날, Time: 89ms, Memory: 9MB, Status: Wrong Answer - Codetree

--- 260122/그 계절, 그 날/that-season-that-day.java
+++ 260122/그 계절, 그 날/that-season-that-day.java
@@ -15,9 +15,9 @@ public class Main {
                     System.out.print("Spring");
                 }else if(6<=m&&m<=8){
                     System.out.print("Summer");
-                }if(9<=m&&m<=11){
+                }else if(9<=m&&m<=11){
                     System.out.print("Fall");
-                }else {
+                }else if(2<=m&&m<=12) {
                     System.out.print("Winter");
                 }
             }else {
