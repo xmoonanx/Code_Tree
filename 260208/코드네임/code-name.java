da9797ed68ff7773c733c90b3e9d96cc2f3acccc
xmoonanx <89209230+xmoonanx@users.noreply.github.com>
2026-02-08 21:48:02 +0900

    Title: 코드네임, Time: 87ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 260208/코드네임/code-name.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+class Sir{
+    String name;
+    int score;
+
+    public Sir(String name, int score){
+        this.name = name;
+        this.score = score;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // Please write your code here.
+        Sir[] sirs = new Sir[5];
+        int min = 0;
+        for(int i =0; i<5; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String name = st.nextToken();
+            int score = Integer.parseInt(st.nextToken());
+            sirs[i] = new Sir(name, score);
+            if(score<min) min = i;
+        }
+
+        System.out.print(sirs[min].name +" "+ sirs[min].score);
+
+    }
+}
