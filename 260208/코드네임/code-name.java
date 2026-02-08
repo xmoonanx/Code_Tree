afe1c50b811d91f518eaf78604a4165eaf24da4b
xmoonanx <89209230+xmoonanx@users.noreply.github.com>
2026-02-08 21:52:56 +0900

    Title: 코드네임, Time: 87ms, Memory: 9MB, Status: Passed - Codetree

--- 260208/코드네임/code-name.java
+++ 260208/코드네임/code-name.java
@@ -16,16 +16,17 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         // Please write your code here.
         Sir[] sirs = new Sir[5];
-        int min = 0;
+        int min = 100;
+        int cnt =0;
         for(int i =0; i<5; i++){
             StringTokenizer st = new StringTokenizer(br.readLine());
             String name = st.nextToken();
             int score = Integer.parseInt(st.nextToken());
             sirs[i] = new Sir(name, score);
-            if(score<min) min = i;
+            if(score<min) {cnt = i; min = score;}
         }
 
-        System.out.print(sirs[min].name +" "+ sirs[min].score);
+        System.out.print(sirs[cnt].name +" "+ sirs[cnt].score);
 
     }
 }
