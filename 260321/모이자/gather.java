cd9af70fd759aabc962362df98671e6f695518cf
xmoonanx <89209230+xmoonanx@users.noreply.github.com>
2026-03-21 15:19:33 +0900

    Title: 모이자, Time: 92ms, Memory: 9MB, Status: Passed - Codetree

--- 260321/모이자/gather.java
+++ 260321/모이자/gather.java
@@ -1,19 +1,22 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+
 public class Main {
     public static final int INT_MAX = Integer.MAX_VALUE;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         int[] a = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            a[i] = sc.nextInt();
+            a[i] = Integer.parseInt(st.nextToken());
         }
         // Please write your code here.
         int res = INT_MAX;
         for(int i =0; i<n; i++){
             int sumDist = 0;
             for(int j =0; j<n; j++){
-                sumDist += Math.abs(j-i) * a[j];
+                sumDist += Math.abs(j-i) * a[j]; // 절댓값
             }
             res = Math.min(res, sumDist);
         }
